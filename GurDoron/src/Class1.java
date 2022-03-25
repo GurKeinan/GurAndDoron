0d2e4ef6628d59bad14e106bbfc4850ebe948fd5
GurKeinan <gur.keinan@campus.technion.ac.il>
2022-03-25 17:20:56 +0300

    adding one more line

--- GurDoron/src/Class1.java
+++ GurDoron/src/Class1.java
@@ -3,11 +3,13 @@ public class Class1 {
 
 
 
-    public static void main (String[]args) {
+    public static void main (String[] args) {
         int gur = 0;
         if (gur == 0) {
             System.out.println("gur is 0");
             System.out.println("Gur is not 0!!!");
+            System.out.println("Trying to commit in intellej");
+
         }
         
 
