2f030058eccc3d26d7a1743a1e2d2aea1efd11e4
Doron1234 <doron.heller@campus.technion.ac.il>
2022-03-25 17:07:02 +0300

    gur is not really 0

--- GurDoron/src/Class1.java
+++ GurDoron/src/Class1.java
@@ -1,3 +1,9 @@
 public class Class1 {
     //This is the first project!!!
+    public static void main (String[]args) {
+        int gur = 0;
+        if (gur == 0) {
+            System.out.println("gur is 0");
+        }
+    }
 }
